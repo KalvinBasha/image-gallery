cacda95652842bd99251ef9f8284b61cdafe87d3
KalvinBasha <kalvi481@gmail.com>
2024-04-07 16:54:21 +0100

    Final Submission

---
+++ app.js
@@ -0,0 +1,65 @@
+const thumbContainer = document.getElementById("thumb-container");
+const displayImage = document.getElementById("display");
+
+let imageIndex = 0;
+
+const images = [
+  {
+    url: "./assets/image1.jpg",
+    alt: "Walkway",
+  },
+  {
+    url: "./assets/image2.jpg",
+    alt: "Building",
+  },
+  {
+    url: "./assets/image3.jpg",
+    alt: "Lanterns",
+  },
+  {
+    url: "./assets/image4.jpg",
+    alt: "Town",
+  },
+  {
+    url: "./assets/image5.jpg",
+    alt: "Temple",
+  },
+];
+
+function createThumbnails() {
+  images.forEach(function (image) {
+    const img = document.createElement("img");
+    img.src = image.url;
+    img.alt = image.alt;
+    thumbContainer.appendChild(img);
+    img.addEventListener("click", function () {
+      createBigImage(image);
+    });
+  });
+}
+
+function createBigImage(image) {
+  displayImage.innerHTML = "";
+  const bigImg = document.createElement("img");
+  bigImg.src = image.url;
+  bigImg.alt = image.alt;
+  displayImage.appendChild(bigImg);
+}
+
+function showNextImage() {
+  imageIndex = (imageIndex + 1) % images.length;
+  createBigImage(images[imageIndex]);
+}
+
+function showPreviousImage() {
+  imageIndex = (imageIndex - 1 + images.length) % images.length;
+  createBigImage(images[imageIndex]);
+}
+
+document
+  .getElementById("prev-btn")
+  .addEventListener("click", showPreviousImage);
+document.getElementById("next-btn").addEventListener("click", showNextImage);
+
+createThumbnails();
+createBigImage(images[0]);
